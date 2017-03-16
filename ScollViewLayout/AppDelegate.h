5ea52f9357f3cc738e779b5835fbc91e0f9eb001
Mr.jiang <414816566@qq.com>
2017-03-16 14:16:31 +0800

    Initial Commit

---
+++ ScollViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScollViewLayout
+//
+//  Created by aimoke on 17/3/16.
+//  Copyright © 2017年 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
